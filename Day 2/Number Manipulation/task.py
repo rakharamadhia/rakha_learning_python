44e7dbdae12faa1b09d19df5d7732ab5779a7c63
rakharamadhia <rakharamadhia@gmail.com>
2025-11-06 16:26:29 +0700

    day 2 finished

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,5 +1,13 @@
-bmi = 84 / 1.65 ** 2
+# bmi = 84 / 1.65 ** 2
+#
+# bmi +=1
+#
+# print(round (bmi,2))
 
-bmi +=1
+age = 12
 
-print(round (bmi,2))
+print(f"I am {age} years old")
+
+print(6 + 4 / 2 - (1 * 2))
+
+a = int("5") / int(2.7)
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,13 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+# tip_percent = tip/100
+# total_tip = bill * tip_percent
+# total_bill = bill + total_tip
+# bill_per_person = round(total_bill/people,2)
+
+bill_per_person = round((bill + (bill*(tip/100))) /people,2)
+
+print(f"Each person should pay: $ {bill_per_person}" )
+
 
